77bd483edb49cea422d66e6ca3343f25d846d45e
chethu3 <chethohaarjm@gmail.com>
2024-12-05 07:41:58 +0530

    3 pages done

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Navbar from './components/Navigation/Navbar';
 import Home from './pages/Home';
 import CarbonCalculator from './components/Calculator/CarbonCalculator';
 import { useThemeStore } from './store/themeStore';
+import { About } from './components/About/About';
+import { Contact } from './components/Contact/Contact';
+import { Toaster } from 'react-hot-toast';
+import { Resources } from './components/Resources/Resources';
 
 function App() {
   const { isDarkMode } = useThemeStore();
@@ -11,12 +15,15 @@ function App() {
   return (
     <Router>
       <div className={isDarkMode ? 'dark' : ''}>
+        <Toaster position="top-right" />
         <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/calculator" element={<CarbonCalculator />} />
-            {/* Add more routes as needed */}
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resources" element={<Resources />} />
           </Routes>
         </div>
       </div>
